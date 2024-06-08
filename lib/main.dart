9460bfc1a95e8a9bd4706a3248c009389105b5bf
raza <abid.anas3@gmail.com>
2024-06-08 13:04:27 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_project/admin_login.dart';
 import 'package:flutter/material.dart';
 import 'listing_screen.dart';
 import 'notification.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: bottom(),
+      home: AdminLogin(),
     );
   }
 }
--- lib/reuseable_container.dart
+++ lib/reuseable_container.dart
@@ -17,23 +17,20 @@ class ReuseableContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(20),
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 150,
+        Container(
+          margin: const EdgeInsets.all(20),
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: 150,
 
-              decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(image: NetworkImage(""))
-              ),
+          decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(image: NetworkImage(""))
+          ),
 
-               child: locationCity == null ? Text("") : Text(locationCity!)
-               ),
-           ] ),
+           child: locationCity == null ? Text("") : Text(locationCity!)
+           ),
         ]);
   }
 }
